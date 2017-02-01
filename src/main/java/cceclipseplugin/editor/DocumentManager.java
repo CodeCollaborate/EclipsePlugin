20935e87b226daa161b8a9e9509b7f86a455c5d3
Benedict Wong <bennydictwong@gmail.com>
2017-02-01 14:31:13 -0500

    Use new file's path
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>

--- src/main/java/cceclipseplugin/editor/DocumentManager.java
+++ src/main/java/cceclipseplugin/editor/DocumentManager.java
@@ -173,13 +173,13 @@ public class DocumentManager implements IFileChangeNotificationHandler {
 				NullProgressMonitor progressMonitor = new NullProgressMonitor();
 				try {
 					if (newFile.exists()) {
-						pm.putFileInWarnList(relativePath.toString(), FileChangeResponse.class);
+						pm.putFileInWarnList(newFile.getFullPath().makeAbsolute().toString(), FileChangeResponse.class);
 						ByteArrayInputStream in = new ByteArrayInputStream(fileContents.getBytes());
 						newFile.setContents(in, false, false, progressMonitor);
 						in.close();
 					} else {
 						// warn directory watching before creating the file
-						pm.putFileInWarnList(relativePath.toString(), FileCreateResponse.class);
+						pm.putFileInWarnList(newFile.getFullPath().makeAbsolute().toString(), FileCreateResponse.class);
 						ByteArrayInputStream in = new ByteArrayInputStream(fileContents.getBytes());
 						newFile.create(in, false, progressMonitor);
 						in.close();
